9ab7643405c5ac2826cd309115468e83d7474614
Mariafsht <133141886+Mariafsht@users.noreply.github.com>
2023-06-06 13:52:52 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -62,6 +62,44 @@ namespace myTiles {
 . . . 2 . . 2 . 2 . 2 . 2 . . 2 
 . . . 2 . . 2 . 2 . 2 . 2 . . 2 
 `, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.castle.tileGrass2,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.stairSouth,sprites.dungeon.collectibleInsignia,sprites.dungeon.darkGroundCenter,sprites.castle.tileGrass1], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`1000100002010101010101010101010101010102020105060606060606060606060401020201070c0c0303030303030c0c0a01020201070c03030303030303030c0a0102020107030303030303030303030a0102020107030303030303030303030a0102020107030303030303030303030a0102020107030303030303030303030a0102020107030303030303030303030a0102020107030303030303030303030a0102020107030303030303030303030a01020201070c03030303030303030c0a01020201070c0c0303030303030c0c0a01020201080b0b0b0b0b0b0b0b0b0b0901020201010101010101010101010101010202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileDarkGrass2,sprites.builtin.forestTiles0,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles7,sprites.builtin.forestTiles5,sprites.builtin.forestTiles6,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.builtin.forestTiles15,sprites.builtin.forestTiles11,sprites.builtin.forestTiles14,sprites.builtin.forestTiles3], TileScale.Sixteen);
+            case "level5":
+            case "level5":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
         }
         return null;
     })
